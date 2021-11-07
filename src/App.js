155e26d167d9e445cde94a47405d6c0b7d865d2c
Nishimwe Elysee <56052588+nishimweelysee@users.noreply.github.com>
2021-11-07 08:48:56 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
-import Routers from './Routes/Routers'
-import { BrowserRouter as Router } from 'react-router-dom'
+import Routers from "./Routes/Routers";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { createBrowserHistory } from "history";
-
-const history = createBrowserHistory();
-import store from './redux/store';
+import store from "./redux/store";
 import { Provider } from "react-redux";
 
+const history = createBrowserHistory();
+
 class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <Router history={history}>
-                    <Routers />
-                </Router>
-            </Provider>
-        )
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Router history={history}>
+          <Routers />
+        </Router>
+      </Provider>
+    );
+  }
 }
 export default App;
--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import FeaturedEvents from '../components/landingPage/featuredEvents';
-import OurClients from '../components/landingPage/ourClients';
-import PrevEvents from '../components/landingPage/prevEvents';
-import Separateline from '../components/landingPage/separateline';
-import ServicesPage from '../components/landingPage/services';
-import WelcomeImages from '../components/landingPage/welcomeImages';
-import PageWrapper from '../components/Layout/UnAuthorizedLayout';
+import React from "react";
+import FeaturedEvents from "../components/landingPage/featuredEvents";
+import OurClients from "../components/landingPage/ourClients";
+import PrevEvents from "../components/landingPage/prevEvents";
+import ServicesPage from "../components/landingPage/services";
+import WelcomeImages from "../components/landingPage/welcomeImages";
+import PageWrapper from "../components/Layout/UnAuthorizedLayout";
 
 function landingPage(props) {
-    const navData = {
-        title: "Let's Live a Happy Life",
-        text: "Sell and buy  event tickets here to millions of potential customers today"
-    }
-    return (
-        <PageWrapper content={navData} className="">
-            <WelcomeImages />
-            <FeaturedEvents />
-            <PrevEvents />
-            <OurClients />
-            <ServicesPage />
-        </PageWrapper>
-    );
+  const navData = {
+    title: "Let's Live a Happy Life",
+    text:
+      "Sell and buy  event tickets here to millions of potential customers today"
+  };
+  return (
+    <PageWrapper content={navData} className="">
+      <WelcomeImages />
+      <FeaturedEvents />
+      <PrevEvents />
+      <OurClients />
+      <ServicesPage />
+    </PageWrapper>
+  );
 }
 
 export default landingPage;
